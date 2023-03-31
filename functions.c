9c904204ac349d09b0d3fd54dcd1181df3fbfd38
echamoina <echamoinaecha@gmail.com>
2023-03-31 01:17:21 -0700

    1.Task

--- functions.c
+++ functions.c
@@ -96,7 +96,6 @@ int print_percent(va_list types, char buffer[],
 	UNUSED(size);
 	return (write(1, "%%", 1));
 }
-
 /************************* PRINT INT *************************/
 /**
  * print_int - Print int
@@ -140,7 +139,6 @@ int print_int(va_list types, char buffer[],
 
 	return (write_number(is_negative, i, buffer, flags, width, precision, size));
 }
-
 /************************* PRINT BINARY *************************/
 /**
  * print_binary - Prints an unsigned number
